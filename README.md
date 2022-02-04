024bed82bd92d70e24e098b0aca0f1f850c594ca
ltetrel <loic.tetrel.pro@gmail.com>
2022-02-04 15:59:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#1. download ukbb zip files using ukbbfetch utility in `$SCRATCH/ukbb_zip_files`
+
+#2. install datalad_ukbb inside a python 3.8 venv (use ukbfetch_surrogate.sh)
+
+#3. clone this repo in your HPC HOME
+```
+git clone https://github.com/ccna-biomarkers/ukbb_scripts.git
+```
+
+#4. copy `ukbb` dir at `$SCRATCH/datasets/ukbb` on compute canada
+
+#5. you can now submit a preprocessing job for one participant with:
+
+```
+sbatch --account=rrg-jacquese --job-name=fmriprep_ukbb_${PARTICIPANT_ID}_%A.job --mail-user=<REDACTED> --output=/scratch/%u/.slurm/fmriprep_ukbb_${PARTICIPANT_ID}_%A.out --error=/scratch/%u/.slurm/fmriprep_ukbb_${PARTICIPANT_ID}_%A.err ${HOME}/ukbb_scripts/fmriprep-slurm_ukbb.bash ${PARTICIPANT_ID}
+```
---
+++ fmriprep-slurm_ukbb.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#SBATCH --time=168:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8000M
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+PARTICIPANT_ID=$1
+
+DATASET_DIR=${SCRATCH}/datasets/ukbb
+OUTPUT_DIR=${DATASET_DIR}
+export SINGULARITYENV_FS_LICENSE=${HOME}/.freesurfer.txt
+export SINGULARITYENV_TEMPLATEFLOW_HOME=/templateflow
+
+module load singularity/3.8
+
+# prepare input directory
+#copying root dataset into local scratch space
+rsync -rltv --info=progress2 --exclude "sub-*" ${DATASET_DIR} ${SLURM_TMPDIR}
+DATASET_TMPDIR=${SLURM_TMPDIR}/ukbb
+#bids-compatible subject data with ukb_datalad
+source ~/.virtualenvs/datalad_ukbb/bin/activate
+datalad create ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID} --force
+cd ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID}
+datalad ukb-init --bids --force ${PARTICIPANT_ID} 20227_2_0 20252_2_0
+datalad ukb-update --merge --force --keyfile "./"
+cd ${SLURM_TMPDIR}
+deactivate
+
+# fmriprep job
+singularity run --cleanenv -B ${DATASET_TMPDIR}:/WORK -B ${HOME}/.cache/templateflow:/templateflow -B /etc/pki:/etc/pki/ \
+    /lustre06/project/6002071/containers/fmriprep-20.2.1lts.sif \
+    -w /WORK/fmriprep_work \
+    --output-spaces MNI152NLin2009cAsym MNI152NLin6Asym \
+    --notrack --write-graph --resource-monitor \
+    --omp-nthreads 1 --nprocs 1 --mem_mb 8000 \
+    --participant-label sub-${PARTICIPANT_ID} --random-seed 0 --skull-strip-fixed-seed \
+    /WORK /WORK/derivatives/fmriprep participant
+fmriprep_exitcode=$?
+
+# save outputs
+rsync -rltv --info=progress ${DATASET_TMPDIR}/fmriprep_work ${OUTPUT_DIR}/fmriprep_sub-${PARTICIPANT_ID}.workdir
+if [ $fmriprep_exitcode -eq 0 ] ; then
+    rsync -rltv --info=progress ${DATASET_TMPDIR}/derivatives/fmriprep/* ${OUTPUT_DIR}
+    rm -r ${DATASET_TMPDIR}/derivatives/fmriprep/*
+fi
+rm -r ${DATASET_TMPDIR}/fmriprep_work
+
+exit $fmriprep_exitcode 
