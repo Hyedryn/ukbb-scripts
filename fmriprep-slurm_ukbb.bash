532e87b26d2459d18927f78466be7e7569852ee8
ltetrel <loic.tetrel.pro@gmail.com>
2022-02-07 13:21:41 -0500

    fixing paths; fmriprep command

--- fmriprep-slurm_ukbb.bash
+++ fmriprep-slurm_ukbb.bash
@@ -22,28 +22,30 @@ rsync -rltv --info=progress2 --exclude "sub-*" ${DATASET_DIR} ${SLURM_TMPDIR}
 DATASET_TMPDIR=${SLURM_TMPDIR}/ukbb
 #bids-compatible subject data with ukb_datalad
 source ~/.virtualenvs/datalad_ukbb/bin/activate
-datalad create ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID} --force
+datalad create ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID}
 cd ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID}
 datalad ukb-init --bids --force ${PARTICIPANT_ID} 20227_2_0 20252_2_0
-datalad ukb-update --merge --force --keyfile "./"
+datalad ukb-update --merge --force --keyfile "./" --drop archives
+rm -r sub-${PARTICIPANT_ID}/*/non-bids
 cd ${SLURM_TMPDIR}
 deactivate
 
 # fmriprep job
-singularity run --cleanenv -B ${DATASET_TMPDIR}:/WORK -B ${HOME}/.cache/templateflow:/templateflow -B /etc/pki:/etc/pki/ \
+singularity run --cleanenv -B ${SLURM_TMPDIR}:/WORK -B ${DATASET_TMPDIR}:/DATA -B ${HOME}/.cache/templateflow:/templateflow -B /etc/pki:/etc/pki/ \
     /lustre06/project/6002071/containers/fmriprep-20.2.1lts.sif \
     -w /WORK/fmriprep_work \
     --output-spaces MNI152NLin2009cAsym MNI152NLin6Asym \
     --notrack --write-graph --resource-monitor \
     --omp-nthreads 1 --nprocs 1 --mem_mb 8000 \
     --participant-label sub-${PARTICIPANT_ID} --random-seed 0 --skull-strip-fixed-seed \
-    /WORK /WORK/derivatives/fmriprep participant
+    /DATA /DATA/derivatives/fmriprep participant
 fmriprep_exitcode=$?
 
 # save outputs
-rsync -rltv --info=progress ${DATASET_TMPDIR}/fmriprep_work ${OUTPUT_DIR}/fmriprep_sub-${PARTICIPANT_ID}.workdir
+rsync -rltv --info=progress ${SLURM_TMPDIR}/fmriprep_work ${OUTPUT_DIR}/fmriprep_sub-${PARTICIPANT_ID}.workdir
 if [ $fmriprep_exitcode -eq 0 ] ; then
-    rsync -rltv --info=progress ${DATASET_TMPDIR}/derivatives/fmriprep/* ${OUTPUT_DIR}
+    rsync -rltv --info=progress ${DATASET_TMPDIR}/derivatives ${OUTPUT_DIR}
+    rsync -rltv --info=progress ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID} ${OUTPUT_DIR}
     rm -r ${DATASET_TMPDIR}/derivatives/fmriprep/*
 fi
 rm -r ${DATASET_TMPDIR}/fmriprep_work
