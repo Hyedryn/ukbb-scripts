66099921b899af0955040b321e71788c54163376
ltetrel <loic.tetrel.pro@gmail.com>
2022-03-25 16:20:43 -0400

    adding extraction scripts; syncing data; tar-ing

--- fmriprep-slurm_ukbb.bash
+++ fmriprep-slurm_ukbb.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --time=24:00:00
+#SBATCH --time=26:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=8000M
+#SBATCH --mem=48G
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
@@ -11,28 +11,42 @@ PARTICIPANT_ID=$1
 
 DATASET_DIR=${SCRATCH}/datasets/ukbb
 OUTPUT_DIR=${DATASET_DIR}
+tar_preprocessed=${PARTICIPANT_ID}_raw_preprocessed.tar.gz
+tar_workdir=${PARTICIPANT_ID}_workdir.tar.gz
 export SINGULARITYENV_FS_LICENSE=${HOME}/.freesurfer.txt
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/templateflow
 
 module load singularity/3.8
 
-# prepare input directory
-#copying root dataset into local scratch space
-rsync -rltv --info=progress2 --exclude "sub-*" --exclude "*.workdir" ${DATASET_DIR} ${SLURM_TMPDIR}
+# copying root dataset into local scratch space
+rsync -rlt --exclude "sub-*" --exclude "*.tar.gz" --exclude "*.workdir" --exclude "derivatives" ${DATASET_DIR} ${SLURM_TMPDIR}
 DATASET_TMPDIR=${SLURM_TMPDIR}/ukbb
+
+### UKBB bids-ification
 #bids-compatible subject data with ukb_datalad
-source ~/.virtualenvs/datalad_ukbb/bin/activate
+source ~/.virtualenvs/datalad-ukbb/bin/activate
 datalad create ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID}
 cd ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID}
 datalad ukb-init --bids --force ${PARTICIPANT_ID} 20227_2_0 20252_2_0
 datalad ukb-update --merge --force --keyfile "./" --drop archives
 rm -r */non-bids
+# fix task names if needed
+for f in $(find . -name "*.json" -type l);
+do 
+    if grep $f -e "TaskName"; then
+    	continue
+    fi
+    PATTERN=".*_task-(.*)_.*"
+    [[ $f =~ $PATTERN ]]
+    JSON_LINE='        "TaskName": "'${BASH_REMATCH[1]}'",'
+    sed -i "2 i\\$JSON_LINE" $f
+done
 cd ${SLURM_TMPDIR}
 deactivate
 
-# fmriprep job
+### Preprocessing
 singularity run --cleanenv -B ${SLURM_TMPDIR}:/WORK -B ${DATASET_TMPDIR}:/DATA -B ${HOME}/.cache/templateflow:/templateflow -B /etc/pki:/etc/pki/ \
-    /lustre06/project/6002071/containers/fmriprep-20.2.1lts.sif \
+    /lustre06/project/6002071/containers/fmriprep-20.2.0lts.sif \
     -w /WORK/fmriprep_work \
     --output-spaces MNI152NLin2009cAsym MNI152NLin6Asym \
     --notrack --write-graph --resource-monitor \
@@ -41,13 +55,21 @@ singularity run --cleanenv -B ${SLURM_TMPDIR}:/WORK -B ${DATASET_TMPDIR}:/DATA -
     /DATA /DATA/derivatives/fmriprep participant
 fmriprep_exitcode=$?
 
-# save outputs
-rsync -rltv --info=progress ${SLURM_TMPDIR}/fmriprep_work ${OUTPUT_DIR}/fmriprep_sub-${PARTICIPANT_ID}.workdir
-if [ $fmriprep_exitcode -eq 0 ] ; then
-    rsync -rltv --info=progress ${DATASET_TMPDIR}/derivatives ${OUTPUT_DIR}
-    rsync -rltv --info=progress ${DATASET_TMPDIR}/sub-${PARTICIPANT_ID} ${OUTPUT_DIR}
-    rm -r ${DATASET_TMPDIR}/derivatives/fmriprep/*
-fi
-rm -r ${DATASET_TMPDIR}/fmriprep_work
+# tar preprocessed outputs
+cd ${DATASET_TMPDIR}
+tar -czvf $tar_preprocessed .
+cd ${SLURM_TMPDIR}/fmriprep_work
+tar -czvf $tar_workdir .
+
+### ts extraction
+rsync -rlt $SCRATCH/atlases $SLURM_TMPDIR/
+source ~/.virtualenvs/ts_extraction/bin/activate
+python3 ~/ccna_ts_extraction/extract_timeseries_tar.py -i $SLURM_TMPDIR/ukbb/derivatives/fmriprep/fmriprep/ --atlas-path $SLURM_TMPDIR/atlases --dataset-name ukbb -o $SLURM_TMPDIR
+deactivate
+
+### tranfer archives
+scp ${SLURM_TMPDIR}/*.tar.gz  ${OUTPUT_DIR}/
+chmod u+rwx -R ${SLURM_TMPDIR}
+rm -rf ${SLURM_TMPDIR}/*
 
 exit $fmriprep_exitcode 
